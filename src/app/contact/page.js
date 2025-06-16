c472d9857cd07b62cdd1c6e641e88ecfdd870a8f
ALI-ADAM94 <aliadam081194@gmail.com>
2025-06-16 20:53:19 +0300

    link tag

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,7 @@
 
 import Navbar from '@/components/Navbar';
 import React from 'react';
+import Link from 'next/link';
 function Contact() {
   return (
     <>
@@ -8,7 +9,7 @@ function Contact() {
       <main className="p-8">
         <h2 className="text-2xl font-bold mb-4">تواصل معي</h2>
         <p>البريد الإلكتروني: aliadam081194@gmail.com</p>
-        <p> رابطGitHub : https://github.com/ALI-ADAM94 </p>
+        <p> رابط :<link href='https://github.com/ALI-ADAM94'>GitHub</link></p>
       </main>
     </>
   );
