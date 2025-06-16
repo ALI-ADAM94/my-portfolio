221b3e06310a616908c578ef50a53039691fa406
ALI-ADAM94 <aliadam081194@gmail.com>
2025-06-16 20:54:54 +0300

    link

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,7 +9,7 @@ function Contact() {
       <main className="p-8">
         <h2 className="text-2xl font-bold mb-4">تواصل معي</h2>
         <p>البريد الإلكتروني: aliadam081194@gmail.com</p>
-        <p> رابط :<link href='https://github.com/ALI-ADAM94'>GitHub</link></p>
+        <p> رابط : <link href='https://github.com/ALI-ADAM94'>GitHub</link></p>
       </main>
     </>
   );
